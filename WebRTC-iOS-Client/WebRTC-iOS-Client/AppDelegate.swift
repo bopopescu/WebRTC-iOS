9995eb6ecccb279dfa6a2b0ed379789fb75ee5b8
Disi A <adis@live.cn>
2017-03-08 20:12:04 -0500

    Adding config class.

--- WebRTC-iOS-Client/WebRTC-iOS-Client/AppDelegate.swift
+++ WebRTC-iOS-Client/WebRTC-iOS-Client/AppDelegate.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-let ENABLE_SSL = true
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -18,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        if ENABLE_SSL {
+        if Config.enableSSL {
             
         }
         return true
@@ -44,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        if ENABLE_SSL {
+        if Config.enableSSL {
         }
     }
 
---
+++ WebRTC-iOS-Client/WebRTC-iOS-Client/Config.swift
@@ -0,0 +1,17 @@
+//
+//  Config.swift
+//  WebRTC-iOS-Client
+//
+//  Created by Innovation on 3/8/17.
+//  Copyright © 2017 Innovation. All rights reserved.
+//
+
+import Foundation
+
+class Config {
+    
+    static let endPoint = "appr.tc"
+    static let enableSSL = true
+    
+    
+}
