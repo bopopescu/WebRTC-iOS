7e0442b28a966f7cd2dfe18f6e236f870d04a328
Disi A <adis@live.cn>
2017-03-08 19:58:26 -0500

    Link bridging headers.

--- WebRTC-iOS-Client/WebRTC-iOS-Client/Bridging.h
+++ WebRTC-iOS-Client/WebRTC-iOS-Client/Bridging.h
@@ -1,7 +1,19 @@
 //
 //  Bridging.h
-//  WebRTC-Demo
+//  WebRTC-iOS-Client
 //
 //  Created by Disi A on 6/7/16.
 //  Copyright © 2016 votebin.com. All rights reserved.
 //
+
+#import <Foundation/Foundation.h>
+#import "ARDAppClient.h"
+#import "ARDMessageResponse.h"
+#import "ARDRegisterResponse.h"
+#import "ARDSignalingMessage.h"
+#import "ARDUtilities.h"
+#import "ARDWebSocketChannel.h"
+
+#import <WebRTC/RTCPeerConnection.h>
+#import <WebRTC/RTCEAGLVideoView.h>
+#import <WebRTC/RTCVideoTrack.h>
